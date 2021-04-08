29344d70e8cec493b9c8d648f78c76f4e2652405
Dixon Pedraza <pedrazadixon@gmail.com>
2021-04-08 10:45:45 -0500

    quito comentarios

--- api.php
+++ api.php
@@ -1,8 +1,5 @@
 <?php
 
-// var_dump(filter_var('https://youtu.be/lnAb9dnQCFE', FILTER_VALIDATE_URL));
-// exit();
-
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 
@@ -10,8 +7,6 @@ if (!isset($_GET['url']))
     exit(json_encode(['error' => 'no url']));
 
 $cmd = 'youtube-dl -f best --geo-bypass --get-url "' . urldecode($_GET['url']) . '" ';
-
-// $cmd = 'youtube-dl -f best --geo-bypass --get-url "ytsearch1:' . $_GET['url'] . '" ';
 
 $shell = trim(shell_exec($cmd));
 
