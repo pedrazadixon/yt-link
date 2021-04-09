e322ebec026ec07753ba0d9ab0516b6d457ee589
Dixon Pedraza <pedrazadixon@gmail.com>
2021-04-08 19:54:08 -0500

    convert_accented_characters

--- api.php
+++ api.php
@@ -3,14 +3,23 @@
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 
+include_once 'foreign_chars.php';
+
 if (!isset($_GET['q']))
     exit(json_encode(['error' => 'no url']));
 
 $q = urldecode($_GET['q']);
 
+    $array_from = array_keys($foreign_characters);
+    $array_to = array_values($foreign_characters);
+    $str =  preg_replace($array_from, $array_to, $dee_track_info["title"] . ' - ' . $dee_track_info["artist"]["name"]);
+
 if (preg_match('/^https?:\/\//', $q)) {
     $cmd = 'youtube-dl -f bestaudio --geo-bypass --get-url "' . $q . '" ';
-} else {
+$array_from = array_keys($foreign_characters);
+$array_to = array_values($foreign_characters);
+$q =  preg_replace($array_from, $array_to, $q);
+
     $cmd = 'youtube-dl -f bestaudio --geo-bypass --get-url "ytsearch1:' . $q . ' music" ';
 }
 
